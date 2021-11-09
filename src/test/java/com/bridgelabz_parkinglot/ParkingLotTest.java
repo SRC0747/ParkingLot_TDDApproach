b4197d208faaac116ac848caecf630bb5405367b
SRC0747 <RjSrc@0747>
2021-11-10 01:38:25 +0530

    Added: Failed to check whether the owner can park the car or not.

--- src/test/java/com/bridgelabz_parkinglot/ParkingLotTest.java
+++ src/test/java/com/bridgelabz_parkinglot/ParkingLotTest.java
@@ -1,7 +1,16 @@
 package com.bridgelabz_parkinglot;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParkingLotTest {
 
+    @Test
+    public void givenAVehicle_WhenParked_ShouldReturnTrue() {
+        ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
+        boolean isParked = parkingLotSystem.park(new Object());
+        Assertions.assertTrue(isParked);
+    }
 }
